12bf0f79af44a043d5331dc8da8950c06c0d52fb
Fred Ouyang <j87890464@gmail.com>
2023-10-23 16:10:43 +0800

    test: negative test cases

--- src/RandomMintNft.sol
+++ src/RandomMintNft.sol
@@ -9,26 +9,27 @@ import "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 
 contract RandomMintNft is ERC721, Ownable {
     uint16 public constant CAPPED_SUPPLY = 500;
-    string private _tokenURI;
-    string private _unrevealedTokenURI;
     uint16 private _totalSupply;
     bool public revealed;
+    address private _vrfV2Consumer;
+    string private _tokenURI;
+    string private _unrevealedTokenURI;
     uint16[] private _remainingTokenIDs;
-    address private _vrfV2Cosumer;
     uint256[] public randomNums;
 
-    constructor(string memory name_, string memory symbol_, string memory unrevealedTokenURI_, string memory tokenURI_, address vrfV2Cosumer_) ERC721(name_, symbol_) Ownable(msg.sender) {
+    constructor(string memory name_, string memory symbol_, string memory unrevealedTokenURI_, string memory tokenURI_, address vrfV2Consumer_) ERC721(name_, symbol_) Ownable(msg.sender) {
         _tokenURI = tokenURI_;
         _unrevealedTokenURI = unrevealedTokenURI_;
-        _vrfV2Cosumer = vrfV2Cosumer_;
-        for (uint16 i = 0; i < 500; i++) {
+        _vrfV2Consumer = vrfV2Consumer_;
+        _remainingTokenIDs.push(1);
+        for (uint16 i = 0; i < CAPPED_SUPPLY - 1; i++) {
             _remainingTokenIDs.push(CAPPED_SUPPLY - i);
         }
     }
 
     function setRandomNums(uint256 _requestID) public onlyOwner {
-        (bool success, bytes memory data) = _vrfV2Cosumer.call(abi.encodeWithSignature("getRequestStatus(uint256)", _requestID));
-        require(success, "_vrfV2Cosumer.getRequestStatus() failed");
+        (bool success, bytes memory data) = _vrfV2Consumer.call(abi.encodeWithSignature("getRequestStatus(uint256)", _requestID));
+        require(success, "_vrfV2Consumer.getRequestStatus() failed");
         (bool fullfilled, uint256[] memory randomWords) = abi.decode(data, (bool, uint256[]));
         require(fullfilled, "not fullfilled");
         randomNums = randomWords;
@@ -46,7 +47,7 @@ contract RandomMintNft is ERC721, Ownable {
     function randomMint(address _to) public returns(uint16) {
         require(++_totalSupply <= CAPPED_SUPPLY, "over mint");
         uint16 _index;
-        if (CAPPED_SUPPLY - _totalSupply > 0) {
+        if ((CAPPED_SUPPLY - _totalSupply) > 0) {
             _index = _roll(CAPPED_SUPPLY - _totalSupply);
         } else {
             _index = 0;
@@ -56,12 +57,6 @@ contract RandomMintNft is ERC721, Ownable {
         _mint(_to, _nextTokenID);
         
         return _nextTokenID;
-    }
-
-    function _roll(uint16 _range) private view returns(uint16) {
-        uint16 _index = (uint16)(randomNums[_totalSupply % 100] % _range);
-
-        return _index;
     }
 
     function totalSupply() public view returns (uint256){
@@ -88,5 +83,11 @@ contract RandomMintNft is ERC721, Ownable {
     function _updateRemainingTokenIDs(uint16 _usedIndex) private {
         _remainingTokenIDs[_usedIndex] = _remainingTokenIDs[_remainingTokenIDs.length - 1];
         _remainingTokenIDs.pop();
+    }
+
+    function _roll(uint16 _range) private view returns(uint16) {
+        uint16 _index = (uint16)(randomNums[_totalSupply % 100] % _range);
+
+        return _index;
     }
 }
--- test/RandomMintNft.t.sol
+++ test/RandomMintNft.t.sol
@@ -8,14 +8,20 @@ import {VRFv2Consumer} from "../src/VRFv2Consumer.sol";
 contract RandomMintNftTest is Test {
     RandomMintNft public randomMintNft;
     address public user1;
-    uint256 sepoliaFork;
+    uint256 public sepoliaFork;
+    uint256 public vrfRequestID;
+    string public tokenURI;
+    string public unrevealedTokenURI;
 
     function setUp() public {
         sepoliaFork = vm.createFork("https://eth-sepolia.g.alchemy.com/v2/dsjALVugZwNUUDVJim7ZLE61WAMpk-7J");
+        vrfRequestID = 2434958413822996221281407648824320680268504669445970203830331396250695750904;
+        tokenURI = "ipfs://bafybeiffsp2cje3lsyagfn6yzpdjfm7n62xmrygnjpu2pq2hczptqqjdnm";
+        unrevealedTokenURI = "ipfs://Qmdc24K1yFzysWTYUHfFbYbNywuggHX1ijqcTCqS4Q35Zz";
         vm.selectFork(sepoliaFork);
         user1 = makeAddr("user1");
         vm.prank(user1);
-        randomMintNft = new RandomMintNft("Random Mint NFT", "RND", "ipfs://Qmdc24K1yFzysWTYUHfFbYbNywuggHX1ijqcTCqS4Q35Zz", "ipfs://bafybeiffsp2cje3lsyagfn6yzpdjfm7n62xmrygnjpu2pq2hczptqqjdnm", 0xF51353449dCc09f9A0D3Ec0eF72a13017D5e6eCF);
+        randomMintNft = new RandomMintNft("Random Mint NFT", "RND", unrevealedTokenURI, tokenURI, 0xF51353449dCc09f9A0D3Ec0eF72a13017D5e6eCF);
     }
 
     function test_Mint() public {
@@ -34,9 +40,19 @@ contract RandomMintNftTest is Test {
         vm.stopPrank();
     }
 
+    function test_Fail_OverMint() public {
+        vm.startPrank(user1);
+        for (uint16 i = 0; i < 500; i++) {
+            randomMintNft.mint(user1);
+        }
+        vm.expectRevert("over mint");
+        randomMintNft.mint(user1);
+        vm.stopPrank();
+    }
+
     function test_RandomMint() public {
         vm.startPrank(user1);
-        randomMintNft.setRandomNums(2434958413822996221281407648824320680268504669445970203830331396250695750904);
+        randomMintNft.setRandomNums(vrfRequestID);
         randomMintNft.randomMint(user1);
         assertEq(randomMintNft.balanceOf(user1), 1);
         vm.stopPrank();
@@ -44,7 +60,7 @@ contract RandomMintNftTest is Test {
 
     function test_RandomBulkMint() public {
         vm.startPrank(user1);
-        randomMintNft.setRandomNums(2434958413822996221281407648824320680268504669445970203830331396250695750904);
+        randomMintNft.setRandomNums(vrfRequestID);
         for (uint16 i = 0; i < 500; i++) {
             randomMintNft.randomMint(user1);
         }
@@ -52,9 +68,20 @@ contract RandomMintNftTest is Test {
         vm.stopPrank();
     }
 
+    function test_Fail_OverRandomMint() public {
+        vm.startPrank(user1);
+        randomMintNft.setRandomNums(vrfRequestID);
+        for (uint16 i = 0; i < 500; i++) {
+            randomMintNft.randomMint(user1);
+        }
+        vm.expectRevert("over mint");
+        randomMintNft.randomMint(user1);
+        vm.stopPrank();
+    }
+
     function test_MixedMint() public {
         vm.startPrank(user1);
-        randomMintNft.setRandomNums(2434958413822996221281407648824320680268504669445970203830331396250695750904);
+        randomMintNft.setRandomNums(vrfRequestID);
         for (uint16 i = 0; i < 250; i++) {
             randomMintNft.mint(user1);
             randomMintNft.randomMint(user1);
@@ -66,10 +93,10 @@ contract RandomMintNftTest is Test {
     function test_TokenURI() public {
         vm.startPrank(user1);
         uint256 tokenId = randomMintNft.mint(user1);
-        assertEq(randomMintNft.tokenURI(tokenId), "ipfs://Qmdc24K1yFzysWTYUHfFbYbNywuggHX1ijqcTCqS4Q35Zz");
+        assertEq(randomMintNft.tokenURI(tokenId), unrevealedTokenURI);
         randomMintNft.toggleRevealStatus();
         assertEq(randomMintNft.revealed(), true);
-        assertEq(randomMintNft.tokenURI(tokenId), "ipfs://bafybeiffsp2cje3lsyagfn6yzpdjfm7n62xmrygnjpu2pq2hczptqqjdnm");
+        assertEq(randomMintNft.tokenURI(tokenId), tokenURI);
         vm.stopPrank();
     }
 }
